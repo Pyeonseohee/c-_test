4f031e1875599f38fda0f59a39358ff37efcf520
Pyeonseohee <58354506+Pyeonseohee@users.noreply.github.com>
2023-02-12 21:37:32 +0900

    using_private

---
+++ basic/chapter04/point/Point.h
@@ -0,0 +1,16 @@
+#ifndef __POINT__H_
+#define __POINT__H_
+
+class Point
+{
+    private:
+        int x; int y;
+
+    public:
+        bool InitMembers(int xpos, int ypos);
+        int GetX() const;
+        int Gety() const;
+        bool SetX(int xpos);
+        bool SetY(int ypos);
+};
+#endif
