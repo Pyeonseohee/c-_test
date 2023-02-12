6e5e4cd4a9242cce83ed391b4295c111040e33fe
Pyeonseohee <58354506+Pyeonseohee@users.noreply.github.com>
2023-02-12 16:21:11 +0900

    using_class_with_access_control_in_c++

--- basic/chapter03/RacingCarClassBase.cpp
+++ basic/chapter03/RacingCarClassBase.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-namespace CAR_CONST{
-    enum{
+namespace CAR_CONST
+{
+    enum
+    {
         ID_LEN = 20,
         MAX_SPD = 200,
         FUEL_STEP = 2,
@@ -11,17 +14,28 @@ namespace CAR_CONST{
     };
 }
 
-struct Car{
-    char gamerID[CAR_CONST::ID_LEN]; // 소유자 id
-    int fuelGauge; // 연료량
-    int curSpeed; //현재속도
-
-    void ShowCarState(); // 상태정보 출력
-    void Accel(); // 엑셀, 속도 증가
-    void Break(); // 브레이크, 속도 감소
+class Car
+{
+    private:
+        char gamerID[CAR_CONST::ID_LEN];
+        int fuelGauge;
+        int curSpeed;
+    public:
+        void InitMembers(char* ID, int fuel);
+        void ShowCarState();
+        void Accel();
+        void Break();
 };
 
-void Car::ShowCarState(){
+void Car::InitMembers(char* ID, int fuel)
+{
+    strcpy(gamerID, ID);
+    fuelGauge = fuel;
+    curSpeed = 0;
+};
+
+void Car::ShowCarState()
+{
     cout << "소유자 ID: " << gamerID << endl;
     cout << "연료량: "<< fuelGauge << '%' << endl;
     cout << "현재속도: "<< curSpeed << "km/s" << endl << endl;
@@ -33,23 +47,30 @@ void Car::Accel(){
     else
         fuelGauge -= CAR_CONST::FUEL_STEP;
 
-    if (curSpeed + CAR_CONST::ACC_STEP >= CAR_CONST::MAX_SPD){
+    if (curSpeed + CAR_CONST::ACC_STEP >= CAR_CONST::MAX_SPD)
+    {
         curSpeed = CAR_CONST::MAX_SPD;
         return ;
     }
     curSpeed += CAR_CONST::ACC_STEP;
 }
 
-void Car::Break(){
-    if (curSpeed < CAR_CONST::BRK_STEP){
+void Car::Break()
+{
+    if (curSpeed < CAR_CONST::BRK_STEP)
+    {
         curSpeed = 0;
         return ;
     }
     curSpeed -= CAR_CONST::BRK_STEP;
 }
 
-int main(void){
-    Car run99 = {"run 99", 100, 0};
+int main(void)
+{
+    Car run99; // 클래스 방식의 초기화
+    run99.InitMembers("run99", 100);
+    run99.Accel();
+    run99.Accel();
     run99.Accel();
     run99.ShowCarState();
     run99.Break();
