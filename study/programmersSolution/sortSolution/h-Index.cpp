6b666de8832df26272f3ef6a2f958f5564162804
Pyeonseohee <voiceinter@naver.com>
2023-03-19 22:56:49 +0900

    Update h-Index.cpp

--- study/programmersSolution/sortSolution/h-Index.cpp
+++ study/programmersSolution/sortSolution/h-Index.cpp
@@ -4,33 +4,25 @@
 #include <algorithm>
 using namespace std;
 
-void printVector(vector<int> v)
-{
-    for(int i = 0; i < v.size(); i++)
-        cout << "v[" << i << "]: " << v[i] << endl;
-}
-
 int solution(vector<int> citations){
     int answer = 0;
     sort(citations.begin(), citations.end());
     if (citations[0] >= citations.size()) return citations.size();
+    
     for(int i = 0; i < citations.size(); i++){
         //citation[i] = h꼴
         int tmp = 0;
         for(int j = 0; j < citations.size(); j++){
-            if (i <= citations[j]){
+            if (i <= citations[j])
                 tmp++;
-            }
-            if(i <= tmp && citations.size() - tmp <= i){
+            if(i <= tmp && citations.size() - tmp <= i)
                 answer = i;
-            }
         }
     }
     return answer;
 }
 
-int main(void)
-{
+int main(void){
     vector<int> citaions = {0, 100, 30, 4, 20, 40, 10, 3, 9, 75};
     cout << solution(citaions) << endl;
     return 0;
