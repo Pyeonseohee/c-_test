9d1f700ee8788a280e0cfc87124f3a82e86227dc
Pyeonseohee <voiceinter@naver.com>
2023-03-19 23:44:01 +0900

    Update Carpet.cpp

--- study/programmersSolution/exhaustiveSolution/Carpet.cpp
+++ study/programmersSolution/exhaustiveSolution/Carpet.cpp
@@ -1,8 +1,19 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
+vector<int> best_factor(int a){
+    vector<int> divisor;
+    if(a <= 0) return ;
+    for(int i = 1; i * i <= a; i++){
+        if(i * i == a){
+            divisor.push_back(i);
+        }else if(a % i == 0){
+            divisor.push_back(i);
+            divisor.push_back(a / i);
+        }
+    }
+}
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
