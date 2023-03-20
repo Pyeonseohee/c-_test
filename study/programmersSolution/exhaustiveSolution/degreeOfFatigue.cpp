23a18a1229fe6d4cf40b26556d85f6616ac11258
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-03-20 23:09:07 +0900

    Update degreeOfFatigue.cpp

--- study/programmersSolution/exhaustiveSolution/degreeOfFatigue.cpp
+++ study/programmersSolution/exhaustiveSolution/degreeOfFatigue.cpp
@@ -1,9 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = -1;
+    sort(dungeons.begin(), dungeons.end());
+    do{
+        for(int i = 0; i < dungeons.size(); i++)
+        {
+            cout << dungeons[i][0] << " " << dungeons[i][1] << endl;
+        }
+        cout << endl;
+    }while(next_permutation(dungeons.begin(), dungeons.end()));
     return answer;
+}
+
+int main(void){
+    vector<vector<int>> dd = {{80, 20}, {50, 40}, {30, 10}};
+    solution(80, dd);
+    return 0;
 }
