cc1e07989f1d327d2c711e5490b1270ce4c99815
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-03-20 22:35:19 +0900

    create degreeOfFatigue.cpp

--- study/Sort/quickSort.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../myLibrary/sorting.cpp" // 정수 swap과 정수 배열 출력
-using namespace std;
-
-int main(void)
-{
-    
-    return 0;
-}
---
+++ study/programmersSolution/exhaustiveSolution/degreeOfFatigue.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+    return answer;
+}
