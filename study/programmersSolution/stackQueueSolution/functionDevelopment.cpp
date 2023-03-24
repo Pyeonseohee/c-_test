b3101ec2c3cdb223b11e94007108235b031721e2
Pyeonseohee <58354506+Pyeonseohee@users.noreply.github.com>
2023-03-24 22:54:49 +0900

    Update functionDevelopment.cpp

--- study/programmersSolution/stackQueueSolution/functionDevelopment.cpp
+++ study/programmersSolution/stackQueueSolution/functionDevelopment.cpp
@@ -3,37 +3,23 @@
 #include <iostream>
 using namespace std;
 
-void printVector(vector<int> v)
-{
-    for(int i = 0; i < v.size(); i++)
-        cout << "v[" << i << "]: " << v[i] << endl;
-}
-
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> count;
-    int tmp = 1;
-    int front  = 0;
-    
+    int day;
+    int max_day = 0;
     for(int i = 0; i < progresses.size(); i++){
-        int progress = 100 - progresses[i];
-        count.push_back(progress / speeds[i]);
-        if(progress % speeds[i] != 0)
-            count[i]++;
-    }
-
-    for(int i = 1; i < count.size(); i++){
-        if(count[front] >= count[i])
-            tmp++;
-        else{
-            answer.push_back(tmp);
-            front = i;
-            tmp = 1;
+        day = ((99 - progresses[i]) / speeds[i]) + 1;
+        if(answer.empty() || day > max_day){
+            answer.push_back(1);
+            max_day = day;
         }
+        else
+            ++answer.back();
     }
-    answer.push_back(tmp);
     return answer;
 } 
+
 int main(void){
     vector<int> p = {95, 90, 99, 99, 80, 99};
     vector<int> s = {1, 1, 1, 1, 1, 1};
