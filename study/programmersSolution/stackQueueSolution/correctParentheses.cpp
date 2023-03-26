0cf785fd14b61a6a32d24bdfb408521cd2cd3aec
Pyeonseohee <58354506+Pyeonseohee@users.noreply.github.com>
2023-03-26 21:01:05 +0900

    not yet

--- study/programmersSolution/stackQueueSolution/correctParentheses.cpp
+++ study/programmersSolution/stackQueueSolution/correctParentheses.cpp
@@ -1,27 +1,24 @@
-#include<string>
+#include <string>
 #include <iostream>
 #include <deque>
 using namespace std;
 
-
-
 bool solution(string s)
 {
     deque<char> str;
-    int len = 0;
     for(char c: s){
         str.push_back(c);
-        len++;
     }
 
-    if(str.front() == ')' || str.back() == '(' || len % 2 != 0) return false;
+    if(str.front() == ')' || str.back() == '(' || s.length() % 2 != 0) return false;
+    
     while(!str.empty()){
-        int q = str.front();
+        char q = str.front();
         str.pop_front();
+        cout << "q: " << q << " " << "front: " << str.front() << endl;
         if(q == str.front()){
             if(q == str.back()) {
                 return false;
-                break;
             }
             str.pop_back();
         }else
@@ -31,7 +28,15 @@ bool solution(string s)
 }
 
 int main(void){
-    string s = "()(())()";
-    cout << solution(s) << endl;
+    string s1 = "()(())()"; // true
+    string s2 = "((())))"; // false
+    string s3 = "(((())))"; // true
+
+    cout << solution(s1) << endl;
+    cout << " ------------------------- " << endl;
+    cout << solution(s2) << endl;
+    cout << " ------------------------- " << endl;
+    cout << solution(s3) << endl;
+    cout << " ------------------------- " << endl;
     return 0;
 }
--- study/programmersSolution/stackQueueSolution/truckOvertheBridge.cpp
+++ study/programmersSolution/stackQueueSolution/truckOvertheBridge.cpp
@@ -7,16 +7,21 @@ using namespace std;
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
     vector<int> ing;
+    int front = 0;
     ing.push_back(truck_weights[0]);
-    for(int i = 1; i < truck_weights.size(); i++){
-        int newTruck = truck_weights[i];
-        int sum = accumulate(ing.begin(), ing.end(), 0);
-        if(sum + newTruck){
-            ing.push_back(newTruck);
-            
-        }
+    while(!ing.empty()){
+        
+
     }
 
 
     return answer;
+}
+
+int main(void){
+    int b = 2;
+    int w = 10;
+    vector<int> truck = {7, 4, 5, 6};
+    solution(b, w, truck);
+    return 0;
 }
