4dcfb412101e514a3043e467e614e5cf0f46ed1a
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-03-27 23:18:22 +0900

    solve correctParentheses

--- study/programmersSolution/stackQueueSolution/correctParentheses.cpp
+++ study/programmersSolution/stackQueueSolution/correctParentheses.cpp
@@ -1,42 +1,35 @@
 #include <string>
 #include <iostream>
-#include <deque>
+#include <queue>
 using namespace std;
 
 bool solution(string s)
 {
-    deque<char> str;
-    for(char c: s){
-        str.push_back(c);
+    queue<char> str;
+    int len = s.length();
+    if(s[0] == ')' || s[len - 1] == '(' || len % 2 != 0) return false;
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == ')' && !str.empty()) str.pop();
+        else if(s[i] == '(') str.push(s[i]);
     }
 
-    if(str.front() == ')' || str.back() == '(' || s.length() % 2 != 0) return false;
-    
-    while(!str.empty()){
-        char q = str.front();
-        str.pop_front();
-        cout << "q: " << q << " " << "front: " << str.front() << endl;
-        if(q == str.front()){
-            if(q == str.back()) {
-                return false;
-            }
-            str.pop_back();
-        }else
-            str.pop_front();
-    }
-    return true;
+    if(str.empty()) return true;
+    else return false;
 }
 
 int main(void){
-    string s1 = "()(())()"; // true
+    string s1 = "()((()))()"; // true
     string s2 = "((())))"; // false
     string s3 = "(((())))"; // true
+    string s4 = "(((())()))"; //true;
 
     cout << solution(s1) << endl;
     cout << " ------------------------- " << endl;
     cout << solution(s2) << endl;
     cout << " ------------------------- " << endl;
     cout << solution(s3) << endl;
+    cout << " ------------------------- " << endl;
+    cout << solution(s4) << endl;
     cout << " ------------------------- " << endl;
     return 0;
 }
