d901e6da152b5b96cd6e53b03433c8de1a624f6d
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-03-28 16:49:46 +0900

    not yet

--- study/programmersSolution/stackQueueSolution/printer.cpp
+++ study/programmersSolution/stackQueueSolution/printer.cpp
@@ -1,9 +1,21 @@
-#include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
+
+    
     return answer;
+}
+
+int main(void){
+    vector<int> p = {2, 1, 3, 2};
+    vector<int> p2 = {9, 1, 1, 1, 1, 1};
+    int l = 2;
+    //solution(p, l);
+    solution(p2, l);
+
+    return 0;
 }
