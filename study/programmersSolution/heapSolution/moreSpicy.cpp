257ccbb78e74c5051b1ffef6125028f267dc6ad0
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-03-28 23:07:11 +0900

    Update moreSpicy.cpp

--- study/programmersSolution/heapSolution/moreSpicy.cpp
+++ study/programmersSolution/heapSolution/moreSpicy.cpp
@@ -1,9 +1,10 @@
 #include <string>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    int answer = 0;
+    int answer = 0; 
     return answer;
 }
