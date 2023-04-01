c573f4bb6f9a2a060b3700ce96149de2c97abc57
Pyeonseohee <voiceinter@naver.com>
2023-04-01 19:43:10 +0900

    solve lifeboat but utility is too low.

--- study/programmersSolution/greedySolution/lifeboat.cpp
+++ study/programmersSolution/greedySolution/lifeboat.cpp
@@ -2,31 +2,30 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <deque>
 using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
     sort(people.begin(), people.end());
-    for(int i = 0; i < people.size(); i++){
-        for(int j = people.size() - 1; j > i; j--){
-            if(people[i] + people[j] < limit){
-                
+
+    while(!people.empty()){
+        vector<int>::iterator iter = people.begin();
+        for(int i = people.size() - 1; i > 0; i--){
+            if(people.front() + people[i] <= limit){
+                people.erase(iter + i);
+                break;
             }
-            
         }
+        people.erase(iter);
+        answer++;
     }
-
-
-
-
     return answer;
 }
 
 int main(void){
     int lim1 = 100;
-    vector<int> p1 = {20, 90, 60, 50, 100};
+    vector<int> p1 = {20, 95, 100, 80, 50, 30, 60};
     vector<int> p2 = {50, 60, 50, 90};
-    solution(p1, lim1);
+    cout << solution(p1, lim1) << endl;
     return 0;
 }
