8d5eb3c4f3b7ea2990561860ee235c4bf0851d0e
Pyeonseohee <voiceinter@naver.com>
2023-04-03 00:24:03 +0900

    not yet

--- study/programmersSolution/hashSolution/Camouflage.cpp
+++ study/programmersSolution/hashSolution/Camouflage.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <set>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
@@ -9,17 +11,41 @@ int solution(vector<vector<string>> clothes) {
     answer += len;
 
     vector<int> count;
+    vector<string> kind;
+    kind.push_back(clothes[0][1]);
+    count.push_back(1);
 
-    for(int i = 0; i < len; i++){
-        // 어떻게 구별해서 넣을건데?
+    for(int i = 1; i < len; i++){
+        string clo = clothes[i][1];
+        auto idx = find(kind.begin(), kind.end(), clo);
+        if(idx == kind.end()) {
+            kind.push_back(clo);
+            count.push_back(1);
+        }else{
+            count[idx - kind.begin()]++;
+        }
     }
 
+    int num = count.size();
+    for(int i = 2; i <= num; i++){
+        vector<bool> temp(num, false);
+        for(int j = 0; j < i; j++)
+            temp[j] = true;
+        do{
+            int sum = 1;
+            for(int j = 0; j < num; j++){
+                if(temp[j])
+                    sum *= count[j];
+            }
+            answer += sum;
+        }while(prev_permutation(temp.begin(), temp.end()));
+    }
     return answer;
 }
 
 int main(void){
-    vector<vector<string>> c1 = {{"yellow_hat", "headgear"}, 
-    {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+    vector<vector<string>> c1 = {{"a", "head"}, 
+    {"b", "eye"}, {"c", "head"}, {"d", "eye"}, {"e", "face"}, {"f", "head"}};
 
     vector<vector<string>> c2 = {{"crow_mask", "face"}, 
     {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
