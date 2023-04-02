2520a026140edf58f76ef5837b248c65482a053d
Pyeonseohee <voiceinter@naver.com>
2023-04-02 17:28:08 +0900

    not yet

---
+++ study/programmersSolution/hashSolution/Camouflage.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    return answer;
+}
+
+int main(void){
+    vector<vector<string>> c1 = {{"yellow_hat", "headgear"}, 
+    {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+    vector<vector<string>> c2 = {{"crow_mask", "face"}, 
+    {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+    solution(c1);
+
+    return 0;
+}
---
+++ study/programmersSolution/heapSolution/diskController.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    return answer;
+}
+
+int main(void){
+    vector<vector<int>> j = {{0, 3}, {1, 9}, {2, 6}};
+    solution(j);
+    return 0;
+}
