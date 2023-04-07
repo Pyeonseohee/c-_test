ee6bd5f5afa896be05818049762f158930f6f16f
Pyeonseohee <58354506+Pyeonseohee@users.noreply.github.com>
2023-04-07 23:44:38 +0900

    not yet

--- study/programmersSolution/graphSolution/theFurthestNode.cpp
+++ study/programmersSolution/graphSolution/theFurthestNode.cpp
@@ -7,3 +7,10 @@ int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
     return answer;
 }
+
+int main(void){
+    int n = 6;
+    vector<vector<int>> vertex = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+    solution(n, vertex);
+    return 0;
+}
