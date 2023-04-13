974e58f071f15591869c13837193986228612d6c
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-04-14 08:58:54 +0900

    not yet

--- study/programmersSolution/DPSolution/theWayToSchool.cpp
+++ study/programmersSolution/DPSolution/theWayToSchool.cpp
@@ -9,3 +9,11 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     
     return answer;
 }
+
+int main(void){
+    int m1 = 4;
+    int n1 = 3;
+    vector<vector<int>> p1 = {{2, 2}};
+    solution(m1, n1, p1);
+    return 0;
+}
