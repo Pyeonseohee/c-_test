1e044b8fbdce3d5bf3969d8bfb2b087641ab447c
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-04-14 08:44:34 +0900

    create DP

---
+++ study/programmersSolution/DPSolution/theWayToSchool.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    
+    return answer;
+}
