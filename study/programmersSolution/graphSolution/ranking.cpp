3f75168aeb0f75f9b194d5db1caf20d530985553
Pyeonseohee <voiceinter@naver.com>
2023-04-16 23:28:01 +0900

    not yet

--- study/programmersSolution/graphSolution/ranking.cpp
+++ study/programmersSolution/graphSolution/ranking.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
+    
     return answer;
+}
+
+int main(void){
+    int n1 = 5;
+    vector<vector<int>> r1 = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+    solution(n1, r1);
+    return 0;
 }
