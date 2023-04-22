ddc1cd1086fee971b445b17e495c78d56d91c452
Pyeonseohee <voiceinter@naver.com>
2023-04-22 18:41:59 +0900

    solve gugudan

---
+++ study/baekjoonSolution/2739_gugudan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int A;
+    cin >> A;
+
+    for(int i = 1; i < 10; i++)
+        cout << A << " * " << i << " = " << A * i << endl;
+    return 0;
+}
