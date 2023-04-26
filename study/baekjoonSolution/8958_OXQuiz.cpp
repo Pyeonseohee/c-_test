f90723bce18ed7cb84dde402c5c69c300fb122cd
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-04-26 23:12:02 +0900

    Create 8958_OXQuiz.cpp

---
+++ study/baekjoonSolution/8958_OXQuiz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+    int a;
+    scanf("%d", &a);
+
+    for(int i = 0; i < a; i++){
+        char s[80];
+        scanf("%s", s);
+        int j = 0;
+        while(s[j] != '\0'){
+            j++;
+        }
+    }
+    return 0;
+}
