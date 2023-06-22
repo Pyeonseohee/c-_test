6e300eda43f0175bd0bbac841d7a9d09454c2789
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-06-22 19:27:09 +0900

    solve runningRace(but timeout)

--- study/programmersSolution/Level1/runningRace.cpp
+++ study/programmersSolution/Level1/runningRace.cpp
@@ -6,16 +6,12 @@ using namespace std;
 vector<string> solution(vector<string> players, vector<string> callings) {
     vector<string> answer;
     for(int i = 0; i < callings.size(); i++){
-        vector<string> tmp;
         for(int j = 0; j < players.size(); j++){
             if(callings[i] == players[j]){
-                tmp.push_back(callings[i]);
-                tmp.push_back(players[j - 1]);
-            }else{
-                tmp.push_back(players[j]);
+                players[j] = players[j - 1];
+                players[j - 1] = callings[i];
             }
         }
-        players = tmp;
     }
     answer = players;
     return answer;
